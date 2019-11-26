ec96aa28f4dfbbeb3a991d8fda0019a8481c9e86
ShaqueYeritsyan <33052654+ShaqueYeritsyan@users.noreply.github.com>
2019-11-26 14:57:31 +0400

    Update find_largest_word_in_dynamic_array_of_strings.cpp

--- find_largest_word_in_dynamic_array_of_strings.cpp
+++ find_largest_word_in_dynamic_array_of_strings.cpp
@@ -4,12 +4,7 @@
 int main(void){
     std::string maxString = "";
     int size = 5;
-    std::string *arr = new std::string[size];
-    arr[0] = "123";
-    arr[1] = "1234";
-    arr[2] = "12345";
-    arr[3] = "123456";
-    arr[4] = "1234567";
+    std::string *arr = new std::string[size] {"123", "1234", "12345", "123456", "1234567"};
     for(int i = 0; i < size; i++) {
         if (arr[i] > maxString) {
             maxString = arr [i];
