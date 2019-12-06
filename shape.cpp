b54bb7993ad888c2fb728a3ae449c63bf9f8dad5
ShaqueYeritsyan <yeritsyanshake@gmail.com>
2019-12-06 17:19:03 +0400

    shape

--- shape.cpp
+++ shape.cpp
@@ -3,41 +3,31 @@
  
 using namespace std;
 
-// Base class
+// Abstract class
 class Shape {
-   public:
-      void seta(double i) {
-         a = i;
-      }
-      void setb(double j) {
-         b = j;
-      }
-      void setc(double k) {
-         b = k;
-      }
- 
-   protected:
-      double a;
-      double b;
-      double c;
-      double r;
+public:
+      virtual int getArea() = 0;
+      virtual int getperimeter () = 0;
 };
 
 // Derived class
 class Rectangle: public Shape {
+   double width;
+   double height;
+
    public:
+      Rectangle(double w, double h) : width(w), height(h) {}
+
       int getArea() { 
-         return (a * b); 
+         return width * height; 
       }
       int getperimeter () {
-         return (a+b+a+b);
+         return 2 * (width + height);
       }
 };
 
 int main(void) {
-   Rectangle Rect;
-   Rect.seta(5);
-   Rect.setb(7);
+   Rectangle Rect(5,7);
    // Print the area of the object.
    cout << "Total area: " << Rect.getArea() << endl;
    cout <<"Perimeter: " <<Rect.getperimeter() << endl;
