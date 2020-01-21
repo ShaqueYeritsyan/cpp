d6389bc349c11678ad7067493478064c6c33cc01
ShaqueYeritsyan <yeritsyanshake@gmail.com>
2020-01-21 15:04:15 +0400

    Binary search

--- binary_search.cpp
+++ binary_search.cpp
@@ -20,36 +20,36 @@ class binarySearch {
     
             // If x is smaller, ignore right half
             else
-                right = mid - 1;
-         }   
+                right = mid - 1;  
+        }
         // if we reach here, then element was
         // not present
-         return -1;
+        std::cout << "\nElement " << number << " is not present in array \n";
+        return -1;
+
     }
 
+    //https://www.tutorialspoint.com/how-to-use-std-sort-to-sort-an-array-in-cplusplus
     int sorting (int arr[], const int size) {
     std::sort(arr, arr+size); 
-    std::cout << "\nArray after sorting using "
-         "default sort is : \n"; 
+    std::cout << "\nArray after sorting using default sort is : \n"; 
     for (int i = 0; i < size; ++i) 
         std::cout << arr[i] << " "; 
-  
     return 0; 
 }
 };
-int main(void)
+
+int main()
 {
     int arr[] = { 2, 4, 1, 10, 40 };
-    int number = 4;
-    int n = sizeof(arr) / sizeof(arr[0]);
+    int elementOfArray = 3;
+    int countOfElements = sizeof(arr) / sizeof(arr[0]);
+
     binarySearch s;
     s.sorting(arr, 5);
-    int result = s.searchElement(arr, 0, n - 1, number);
-    if (result == -1) {
-        std::cout << "\nElement is not present in array";
-    }
-    else {
-        std::cout << "\nElement is present at index " << result << "\n";
+    int result = s.searchElement(arr, 0, countOfElements - 1, elementOfArray);
+    if (result != -1) {
+        std::cout << "\nElement " <<elementOfArray << " is present at index " << result << "\n";
     }
     return 0;
 }
