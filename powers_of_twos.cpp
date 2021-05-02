836cc388710480e13eff7dcd33f6d6474433cf4e
ShaqueYeritsyan <yeritsyanshake@gmail.com>
2021-05-02 12:15:42 +0400

    powers of twos

---
+++ powers_of_twos.cpp
@@ -0,0 +1,68 @@
+/* A positive integer x is called a power of two if it can be represented as x=2^y, where y is a non-negative integer.
+So, the powers of two are 1,2,4,8,16,…
+
+You are given two positive integers n and k.
+Your task is to represent n as the sum of exactly k powers of two.
+
+Input:
+The only line of the input contains two integers n and k (1<=n<=10^9, 1<=k<=2*⋅10^5).
+
+Output:
+If it is impossible to represent n as the sum of k powers of two, print NO.
+Otherwise, print YES, and then print k positive integers such that each is a power of two
+If there are multiple answers, you may print any of them.
+
+Input: 9, 4 Output: YES, 1, 2, 2, 4
+Input: 8, 1 Output: YES, 8
+Input: 5, 1 Output: NO
+Input: 3, 7 Output: NO
+*/
+
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+void powerOfTwos(int n, int k)
+{
+    int k_min = 0;
+    int bin = n;
+        while(bin > 0) {
+        //convert decimal number to binary and count sum of binary digits to get the min value of k.
+        k_min = k_min+(bin % 2);
+        bin = bin/2;
+    }
+    if (k < k_min || k > n) {
+        std::cout << "NO \n";
+        return;
+    }
+    int p = n;
+    std::vector<int> result;
+    while (p >= 0 && k) {
+        int val = pow(2, p);
+        //std::cout << "p = " << p << " \n";
+        if (n - val < k - 1) {
+            p = p - 1;
+            //std::cout<<"val = "<<val<<" \n";
+            //std::cout << "p = " << p << " \n";
+            continue;
+        	}
+        k = k - 1;
+        //std::cout << "k = " << k << " \n";
+        n = n - val;
+        //std::cout << "n = "<< n << " \n";
+        //std::cout << p << " power of two \n";
+        result.push_back(p);
+    }
+    std::cout<<"YES ";
+    for (int powers : result)
+        std::cout << pow(2, powers) << " ";
+    	std::cout << " \n";
+}
+
+int main () {
+    powerOfTwos(9,4);
+    powerOfTwos(8, 1);
+    powerOfTwos(5, 1);
+    powerOfTwos(3, 7);
+    return 0;
+}
